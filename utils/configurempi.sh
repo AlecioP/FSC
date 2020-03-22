4628b55123d1fc66e61b6858719faf48a888d9a6
AlecioP <alecioportaro@gmail.com>
2020-03-22 18:16:28 +0100

    mpi lib configuration

---
+++ utils/configurempi.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+CURRENT=$(pwd)
+
+curl  http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz --output archive.tar.gz
+tar xfz archive.tar.gz
+mv mpi* mpich
+cd mpich
+mkdir ../bin
+mkdir ../bin/mpi
+
+PREF="${CURRENT}/bin/mpi"
+
+./configure --prefix=${PREF}
+make -j4
+make -j4 install
+
+cd ..
+rm -R mpich
+rm archive.tar.gz
+exit
